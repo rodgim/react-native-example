e2dd916294ea893f128ca680cec6bdf2586ade27
Rodrigo Gimenez <rodrigo05101990@gmail.com>
2021-02-05 14:03:06 -0300

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,6 +4,8 @@ import { Icon, Input, CheckBox, Button } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+import { Asset } from 'expo';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -144,9 +146,23 @@ class RegisterTab extends Component{
 
             if(!capturedImage.cancelled){
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imageUri,
+            [
+                {resize: {width: 400}}
+            ],
+            {
+                format: 'png'
+            }
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
     }
 
     static navigationOptions = {
